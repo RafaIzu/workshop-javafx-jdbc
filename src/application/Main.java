cc3bd8e46a6a6ccea96c7bc4e788ebd4b88e03ee
RafaIzu <bomizukawa@gmail.com>
2020-03-01 17:42:00 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,6 +11,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -20,7 +22,7 @@ public class Main extends Application {
 			scrollpane.setFitToHeight(true);
 			scrollpane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollpane);
+			mainScene = new Scene(scrollpane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -28,8 +30,11 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-
 	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
